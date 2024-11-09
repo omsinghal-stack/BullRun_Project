b2ab6c3bc52a6ac219f341cb5076428e7ace6a93
Om Singahl <142425555+Om-Singhal-stack@users.noreply.github.com>
2024-11-09 19:31:12 +0000

    r2_client

--- client/src/Home.jsx
+++ client/src/Home.jsx
@@ -1,5 +1,6 @@
 import bull_img from '/bull-removebg-preview.png';
 import { useNavigate } from 'react-router-dom';
+import logo from '/bullrun_logo.png';
 import 'bootstrap/dist/css/bootstrap.min.css'
 const Home=()=>{
     const navigate = useNavigate();
@@ -8,8 +9,10 @@ const Home=()=>{
     } 
     return(
         <>
-     
+        <div>
+      <img src={logo} alt="" className="rounded-5 m-1 "/>
       <button type="button" onClick={navToLogin} className="btn btn-outline-primary">Click</button>
+      </div>
         </>
     )
 }
