293168f09468d09b69398c1678c3186f68554068
Om Singahl <142425555+Om-Singhal-stack@users.noreply.github.com>
2024-11-13 06:59:13 +0000

    r5

--- client/src/Home.jsx
+++ client/src/Home.jsx
@@ -2,7 +2,11 @@
 import { useNavigate } from 'react-router-dom';
 import Hero from './home_comp/Hero.jsx'
 import Stats from './home_comp/Stats.jsx'
-import logo from '/bullrun_logo.png';
+import Pricing from './home_comp/Pricing.jsx'
+import Education from './home_comp/Education.jsx'
+import OpenAccount from './home_comp/OpenAccount.jsx'
+
+import logo from '/BullLogo.png';
 import './index.css'
 const Home=()=>{
     const navigate = useNavigate();
@@ -14,13 +18,16 @@ const Home=()=>{
           <div id='home-navbar'>
       <div id='home-navbar-divlogo'>
       <img src={logo} alt="" id='home-navbar-divlogo-img'/>
-      <h2>BULLRUN</h2>  
+        
         </div>      
       <button type="button" id='home-navbar-button' className='button' onClick={navToLogin}>LogIn/SignUp</button>
       </div>
       <Hero/>
       
       <Stats/>
+      <Pricing/>
+      <Education/>
+      <OpenAccount/>
         </>
     )
 }
---
+++ client/src/home_comp/OpenAccount.jsx
@@ -0,0 +1,15 @@
+const OpenAccount=()=>{
+ return (
+    <>
+    <div>
+    <div>
+        <h1>Open a Zerodha account</h1>
+        <p>Modern platforms and apps, ₹0 investments, and flat ₹20 intraday and F&O trades.</p>
+        <button>Signup For Free</button>
+    </div>
+</div>
+
+    </>
+ )
+}
+export default OpenAccount
