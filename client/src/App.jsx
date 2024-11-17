b22a03b5ff56b766284973bb92731907a1415c6f
Om Singahl <142425555+Om-Singhal-stack@users.noreply.github.com>
2024-11-17 18:57:19 +0000

    r10

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,6 +1,7 @@
 import {BrowserRouter,Routes,Route} from 'react-router-dom'
 import LogIn from '../components/LogIn.jsx';
 import SignUp from '../components/SignUp.jsx';
+import Explore from './Explore.jsx'
 import Home from './Home.jsx';
 const App=()=>{
  return(
@@ -9,7 +10,8 @@ const App=()=>{
     <Routes>
     <Route path="/" element={<Home/>}/>
     <Route path="/login" element={<LogIn/>}/>
-    <Route path="/signup" element={<SignUp/>}/>   
+    <Route path="/signup" element={<SignUp/>}/> 
+    <Route path="/explore" element={<Explore/>}/> 
     </Routes>
     </BrowserRouter>
     </>
--- client/src/Explore.jsx
+++ client/src/Explore.jsx
@@ -1,7 +1,29 @@
+import axios from 'axios'
+import { useEffect, useState } from 'react';
 const Explore = ()=>{
+   const API = "https://financialmodelingprep.com/api/v3/symbol/NASDAQ?apikey=POx9PiDc9nnBzmbrjUtIVbmvLECjGQ45";
+   const [StockData,setStockData] = useState([]);
+   const [FilterData,setFilterData] = useState([]);
+   const getStockData=async()=>{
+       try{
+           const res =await axios.get(API);
+           setStockData(res.data);
+           setFilterData (StockData.filter((st)=>{
+               return st.volume>=0
+            }))
+            console.log(filter);
+       }catch(err){
+          console.log(err);
+       }
+   }
+   
+   useEffect(()=>{
+        getStockData();
+   },[])
     return (
         <>
+                  
         </>
     )
 }
-export default Export;
+export default Explore;
