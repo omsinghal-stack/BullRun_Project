24fcd6979741d69945e69d29bd45b7ae90ffa023
Ritakshi Singh <109134641+Ritakshi19@users.noreply.github.com>
2024-11-17 22:27:10 +0530

    Add files via upload

---
+++ client/src/home_comp/Hero.jsx
@@ -0,0 +1,18 @@
+import './home_comp.css';
+const Hero=()=>{
+    return (
+        <>
+        
+            <div id="hero">
+                <img src='Images/homeHero.png' alt='Hero Image' />
+                <h1 >Invest in everything</h1>
+                <p>Online platform to invest in stocks, derivatives, mutual funds, ETFs, bonds, and more.</p>
+                <button >GET STARTED</button>
+            </div>
+        
+
+        </>
+    )
+}
+
+export default Hero
---
+++ client/src/home_comp/OpenAccount.jsx
@@ -0,0 +1,21 @@
+import './home_comp.css';
+import { useNavigate } from "react-router-dom"
+const OpenAccount=()=>{
+    const navigate = useNavigate();
+    const RedirectToSignup=()=>{
+        navigate('/signup');
+    }
+ return (
+    <>
+    <div>
+    <div id="hero">
+        <h1>Open a Zerodha account</h1>
+        <p>Modern platforms and apps, ₹0 investments, and flat ₹20 intraday and F&O trades.</p>
+        <button onClick={RedirectToSignup}>Signup For Free</button>
+    </div>
+</div>
+
+    </>
+ )
+}
+export default OpenAccount
