75c77c4ca83a1ae54e3376899cc5fc98a7cfe57a
Om Singahl <142425555+Om-Singhal-stack@users.noreply.github.com>
2024-11-18 19:53:39 +0000

    r11

--- client/src/Explore.jsx
+++ client/src/Explore.jsx
@@ -1,28 +1,64 @@
 import axios from 'axios'
 import { useEffect, useState } from 'react';
-const Explore = ()=>{
-   const API = "https://financialmodelingprep.com/api/v3/symbol/NASDAQ?apikey=POx9PiDc9nnBzmbrjUtIVbmvLECjGQ45";
-   const [StockData,setStockData] = useState([]);
-   const [FilterData,setFilterData] = useState([]);
-   const getStockData=async()=>{
-       try{
-           const res =await axios.get(API);
-           setStockData(res.data);
-           setFilterData (StockData.filter((st)=>{
-               return st.volume>=0
-            }))
-            console.log(filter);
-       }catch(err){
-          console.log(err);
-       }
-   }
-   
-   useEffect(()=>{
+import Services from './explore_comp/Services.jsx';
+import Loading_Spinner from '../components/Loading_Spinner';
+// import Dedicated from './explore_comp/Dedicated.jsx'
+const Explore = () => {
+    const API = "https://financialmodelingprep.com/api/v3/stock/list?apikey=7tb1GX4qTBrrwWjhi0m97bdp2AVJES4g";
+    const [StockData, setStockData] = useState([]);
+    const [EtfData, setEtfData] = useState([]);
+    const [Trust, setTrust] = useState([]);
+    const [Loading, setLoading] = useState(true);
+    const [midcap,setMidcap] = useState([]);
+    const getStockData = async () => {
+        try {
+            const res = await axios.get(API);
+
+            const st_filt = res.data.filter((st) => {
+                return st.exchangeShortName === 'NASDAQ' && st.type === 'stock'
+            })
+            const tru_filt = res.data.filter((st) => {
+                return st.exchangeShortName === 'NASDAQ' && st.type === 'trust'
+            })
+            const etf_filt = res.data.filter((st) => {
+                return st.exchangeShortName === 'NASDAQ' && st.type === 'etf'
+            })
+            
+            setStockData(st_filt);
+            setEtfData(etf_filt);
+            setTrust(tru_filt);
+            setLoading(false);
+        } catch (err) {
+            console.log(err);
+            setLoading(false);
+        }
+    }
+
+    useEffect(() => {
         getStockData();
-   },[])
+    }, [])
     return (
         <>
-                  
+            {Loading ?
+                <Loading_Spinner />
+                :
+                <>
+                    <Services />
+                    <div>
+                        {StockData.map((st, index) => (
+
+                            <div className='card' key={index}>
+                                <h1>{st.symbol}</h1>
+                                <p>{st.name}</p>
+                                <h3>{st.price}</h3>
+                            </div>
+                        ))}
+                    </div>
+                </>
+
+
+            }
+
         </>
     )
 }
---
+++ client/src/explore_comp/Services.jsx
@@ -0,0 +1,26 @@
+import './explore_comp.css'
+const Services=()=>{
+  return (
+    <>
+
+            {/* service's section */}
+            <div> 
+               <div>
+                {/*use a Investment logo or image */}
+                <img src="" alt="" />
+               <h2>Investment Trust</h2>
+               </div>
+               <div>
+                {/*use a etf logo or image */}
+                <h2>Etf</h2>
+               </div>
+               <div>
+                {/*use a stocks logo or image */}
+                <h2>Stocks</h2>
+               </div>
+            </div>
+
+    </>
+  )
+}
+export default Services;
