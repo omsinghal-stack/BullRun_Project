824da8f2cbd25011a74dae3524dcb2a096bdfd1f
Ritakshi Singh <109134641+Ritakshi19@users.noreply.github.com>
2024-11-20 18:08:09 +0530

    Update Services.jsx

--- client/src/explore_comp/Services.jsx
+++ client/src/explore_comp/Services.jsx
@@ -2,23 +2,28 @@ import './explore_comp.css'
 const Services=()=>{
   return (
     <>
-
             {/* service's section */}
-            <div> 
-               <div>
+            
+            <div className="service"> 
+            <h1>Services Provided</h1>
+            <div className="card1">
+               <div className="services" >
                 {/*use a Investment logo or image */}
-                <img src="" alt="" />
-               <h2>Investment Trust</h2>
+                <img src="logo\investment.png" alt=""/>
+               <h3>Investment Trust</h3>
                </div>
-               <div>
+               <div className="services">
+               <img src="logo\etf.png" alt="" />
                 {/*use a etf logo or image */}
-                <h2>Etf</h2>
+                <h3>Etf</h3>
                </div>
-               <div>
+               <div className="services">
+               <img src="logo\stocks.png" alt="" />
                 {/*use a stocks logo or image */}
-                <h2>Stocks</h2>
+                <h3>Stocks</h3>
                </div>
-            </div>
+               </div>
+            </div><br/>
 
     </>
   )
